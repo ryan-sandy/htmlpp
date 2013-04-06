4c718cf940ac428c24a14571fb423722633c4789
Ryan Lee <ryan@ryansandy.com>
2013-04-06 13:34:36 -0700

    v1 release. Bare-bones

--- README.md
+++ README.md
@@ -1,4 +1,47 @@
-onePage
+﻿onePage
 =======
 
 OnePage is a html preprocessor that glues smaller html pages into one.
+
+Usage
+====
+
+```
+onePage.js [options] [file ...]
+Options:
+-o [output filename]; default screen
+-open [openDelimiter]; default <%
+-close [closeDelimiter]; default %>
+-inc [include Key Word]; default include 
+-v; verbose
+-h; help file
+```
+
+Example
+====
+
+```
+$cat world.html
+<span>World</span>
+$cat hello.html
+<p>Hello <% include world %></p>
+$onePage hello.html
+<p>Hello <span> World </span></p>
+```
+
+Notes
+====
+
+* Html tags are not required.
+* Tags are white space sensitive. (`<% include !== <%include`)
+
+License
+====
+
+Copyright (C) 2013 Ryan Lee
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ onePage.js
@@ -0,0 +1,128 @@
+#!/usr/bin/env node
+
+//Copyright 2013 Ryan Lee
+
+'use strict';
+/*jslint node:true, indent:2, nomen:true*/
+
+var fs = require('fs');
+
+var opts = process.argv.slice(2),
+  i = 0,
+  set = {
+    'outfile' : null,
+    'files' : [],
+    'ext' : '.html',
+    'open' : '<% ',
+    'close' : ' %>',
+    'include' : 'include ',
+    'echo' : false
+  },
+  baseDir = process.cwd(),
+  search = '',
+  output = '';
+
+var parse = function (raw) {
+  var start = raw.indexOf(search),
+    stop = 0,
+    rep = '',
+    include = '',
+    fname = '',
+    fnameExt = '';
+
+  while (start !== -1) {
+    stop = raw.indexOf(set.close, start);
+    fname = raw.slice(start + search.length, stop);
+    if (fname.split('.').length === 1) {
+      fnameExt = fname + set.ext;
+    } else {
+      fnameExt = fname;
+    }
+    if (set.echo) {
+      console.log('Including ' + fnameExt);
+    }
+    include = parse(fs.readFileSync(fnameExt, 'utf8').trim());
+    rep = search + fname + set.close;
+    if (raw.indexOf(rep + '\n') !== -1) {
+      raw = raw.replace(rep + '\n', include);
+    } else {
+      raw = raw.replace(rep, include);
+    }
+    start = raw.indexOf(search);
+  }
+  return raw;
+};
+
+var printHelp = function () {
+  console.log('Pre-processes html files.');
+  console.log('Options:');
+  console.log('-o [output filename]; default screen');
+  console.log('-open [openDelimiter]; default <%');
+  console.log('-close [closeDelimiter]; default %>');
+  console.log('-inc [include Key Word]; default include ');
+  console.log('-v; verbose');
+};
+
+for (i = 0; i < opts.length; i += 1) {
+  if (opts[i][0] === '-') {
+    switch (opts[i][1]) {
+    case 'o':
+      i += 1;
+      set.outfile = opts[i];
+      break;
+    case 'open':
+      i += 1;
+      set.open = opts[i];
+      break;
+    case 'close':
+      i += 1;
+      set.close = opts[i];
+      break;
+    case 'ext':
+      i += 1;
+      set.ext = opts[i];
+      break;
+    case 'inc':
+      i += 1;
+      set.include = opts[i];
+      break;
+    case 'v':
+      set.echo = true;
+      break;
+    case 'h':
+      printHelp();
+      break;
+    default:
+      console.log('Error: Unknown option ' + opts[i]);
+      process.exit(1);
+      break;
+    }
+  } else {
+    set.files.push(opts[i]);
+  }
+}
+
+search = set.open + set.include;
+
+set.files.forEach(function (f) {
+  if (set.echo) {
+    console.log('Compiling file:' + f);
+  }
+  var path, pos, name;
+  pos = f.lastIndexOf('/');
+  if(pos !== -1) {
+    path = f.slice(0, pos);
+    name = f.slice(pos + 1);
+    process.chdir(path);
+  } else {
+    name = f;
+  }
+  output += parse(fs.readFileSync(name, 'utf8'));
+  process.chdir(baseDir);
+});
+
+if (set.outfile) {
+  fs.writeFileSync(set.outfile, output, 'utf8');
+} else {
+  console.log(output);
+}
